0dced0573ec871feef7d779257377f5e5d312abc
Subham Dhar <43316699+subham934@users.noreply.github.com>
2024-09-13 20:07:22 +0530

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+/*=============== SHOW MENU ===============*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+/*=============== REMOVE MENU MOBILE ===============*/
+
+/*=============== HOME SWIPER ===============*/
+var swiper = new Swiper(".home-swiper", {
+  spaceBetween: 30,
+  loop: "true",
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+/*=============== DEALS TAB ===============*/
+
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[content]");
+
+tabs.forEach((tab) => {
+  tab.addEventListener("click", () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tabContents.forEach((tabContent) => {
+      tabContent.classList.remove("active-tab");
+    });
+
+    target.classList.add("active-tab");
+
+    tabs.forEach((tab) => {
+      tab.classList.remove("active-tab");
+    });
+
+    tab.classList.add("active-tab");
+  });
+});
+
+/*=============== SHOW SCROLL UP ===============*/
